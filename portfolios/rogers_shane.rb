b5fd239e7e24f38fbc5fc75d6210e3bca28c714c
Shane Rogers <rogerssh@tcd.ie>
2013-06-25 08:48:18 -0600

    skype name added

--- portfolios/rogers_shane.rb
+++ portfolios/rogers_shane.rb
@@ -2,6 +2,7 @@
   :last_name => "Rogers",
   :first_name => "Shane",
   :email_address => "rogerssh@tcd.ie",
+  :skype_username => "sdrogers_88",
   :looking_for => "An environment where I can be mentored which has an exceptional group culture that drives the highest standards of product development. A company that puts equal emphasis on team communication and best development practices.",
   :best_at => ["Design intelligence", "Communication", "Teamwork"],
   :looking_for => ["TDD", "Pair Programming", "Mentorship"],
